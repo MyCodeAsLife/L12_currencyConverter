e3d125b880237f9356a274d22e1443747149557b
Vladimir <w112vovan@mail.ru>
2023-11-23 16:46:43 +0500

    Добавьте файлы проекта.

---
+++ L12_currencyConverter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12_currencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CommandExit = "exit";
+            const string CommandRubToUsd = "1";
+            const string CommandRubToEur = "2";
+            const string CommandEurToUsd = "3";
+            const string CommandEurToRub = "4";
+            const string CommandUsdToRub = "5";
+            const string CommandUsdToEur = "6";
+
+            float rubToUsd = 88.15f;
+            float rubToEur = 96.26f;
+            float eurToUsd = 0.92f;
+            float eurToRub = 0.01f;
+            float usdToRub = 0.11f;
+            float usdToEur = 1.09f;
+            bool isOpen = true;
+            float rubleWallet;
+            float dollarWallet;
+            float euroWallet;
+            float amountOfMoneyToConvert;
+
+            while (isOpen)
+            {
+
+            }
+        }
+    }
+}
